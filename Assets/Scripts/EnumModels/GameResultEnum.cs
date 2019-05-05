aa5a27ef92724ed8bef7879aeaabb9835c95f08b
OZcomingFRoo <OZcomingFRoo@gmail.com>
2019-05-05 11:48:14 +0300

    Reset project
     - Removed all project's files
     - Modfied gitignore

--- Assets/Scripts/EnumModels/GameResultEnum.cs
+++
@@ -1,7 +0,0 @@
-﻿/// <summary>
-/// Enum that shows if player either won,lost or got a tie with opponent
-/// </summary>
-public enum GameResultEnum
-{
-    Lost , Tie , Won
-}
--- Assets/Scripts/EnumModels/RockPaperScissorsEnum.cs
+++
@@ -1,7 +0,0 @@
-﻿/// <summary>
-/// Translates players input into a move in rock paper scissors game
-/// </summary>
-public enum RockPaperScissorsEnum 
-{
-    UnknownMove = -1 ,Rock = 0 , Paper = 1 , Scissors = 2
-}
